0eb90e63ed568058a70ab7b8468f52585771e696
NotNone <yuzhiqiang508384097@gmail.com>
2025-04-12 12:57:29 +0800

    Create home page

---
+++ src/components/home.rs
@@ -0,0 +1,8 @@
+use dioxus::prelude::*;
+
+#[component]
+pub fn Home() -> Element {
+    rsx! {
+        "Welcome to Home Page"
+    }
+}
---
+++ src/components/mod.rs
@@ -0,0 +1,3 @@
+pub mod home;
+pub mod navbar;
+pub mod not_found;
